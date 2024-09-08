8bbffb7742cfe53f61dc340c08f41488fb8fa371
fbablu <fardeeneb@gmail.com>
2024-09-08 00:18:14 -0500

    early stages

--- main.js
+++
@@ -1,8 +0,0 @@
-/** may be useful in formatting the phone numbers: 
- * https://stackoverflow.com/questions/55331214/how-can-i-format-all-phone-numbers-in-json-array-before-returning-it
- */
-
-
-// taste of nashville: https://campusdining.vanderbilt.edu/taste-of-nashville/
-
-
